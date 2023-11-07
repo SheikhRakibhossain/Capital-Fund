21fc2c82b40329ffd8ddc945c240024277cf7423
Apple <apple@Apples-MacBook-Pro.local>
2023-11-07 21:57:24 +0600

    all are added

--- src/Pages/Home/HomeCom/Hero.jsx
+++ src/Pages/Home/HomeCom/Hero.jsx
@@ -1,6 +1,7 @@
 
 const Hero = () => {
     return (
+        <>
         <div className="hero min-h-screen" style={{backgroundImage: 'url(https://img.freepik.com/free-photo/this-is-your-booking-trip-young-man-planning-vacation-about-sign-purchase-agreement-with-travel-agent_662251-2163.jpg?w=1380&t=st=1699362954~exp=1699363554~hmac=2d36a1caa1a8dbf19445278e86802457c7476199346dfcb43aea64ba6f3a4c88)'}}>
         <div className="hero-overlay bg-opacity-30"></div>
         <div className="hero-content text-center text-neutral-content">
@@ -11,6 +12,38 @@ const Hero = () => {
           </div>
         </div>
       </div>
+      {/* slider part */}
+      <div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full">
+    <img src="/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle">❮</a> 
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" className="carousel-item relative w-full">
+    <img src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" className="btn btn-circle">❮</a> 
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" className="carousel-item relative w-full">
+    <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" className="btn btn-circle">❮</a> 
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide4" className="carousel-item relative w-full">
+    <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" className="btn btn-circle">❮</a> 
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+        </>
     );
 };
 
